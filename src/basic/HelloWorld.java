76d337d3b51a7d5670060c9008bbee8368cacfa2
nvtreal <vanlocgroups@gmail.com>
2024-09-06 23:00:07 +0700

    Update HelloWorld.java

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -10,4 +10,11 @@ public class HelloWorld {
  public void m1() {
 	 System.out.println("From m1");
  }
+	//demo for b1 branch
+ public void m2() {
+	 System.out.println("From m2");	
+}
+	//from d1 branch by dev-2
+	 public void m4() {
+	 System.out.println("From m2");
 }
